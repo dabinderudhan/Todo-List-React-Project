69998499e8ea6b86c0a240344905c1213aca02f5
[dabinderudhan] <[dabinder.udhan@gmail.com]>
2022-07-31 18:40:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  const inputTask = useRef(null);
+
+  const addTask = () => {
+    setTodoList([...todoList, { task: currentTask, completed: false }]);
+    inputTask.current.value = "";
+    setCurrentTask("");
+  };
+
+  const deleteTask = (taskToDelete) => {
+    setTodoList(todoList.filter((task) => task.task !== taskToDelete));
+  };
+
+  const completeTask = (taskToComplete) => {
+    setTodoList(
+      todoList.map((task) => {
+        return task.task === taskToComplete
+          ? { task: taskToComplete, completed: true }
+          : { task: task.task, completed: task.completed ? true : false };
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Task..."
+          ref={inputTask}
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) addTask();
+          }}
+          onChange={(e) => setCurrentTask(e.target.value)}
+        ></input>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <hr />
+      <ul>
+        {todoList.map((val, key) => {
+          return (
+            <div id="task" key={key}>
+              <li>{val.task}</li>
+              <button onClick={() => completeTask(val.task)}>Completed</button>
+              <button onClick={() => deleteTask(val.task)}>Delete</button>
+              {val.completed ? (
+                <h2>Task Completed</h2>
+              ) : (
+                <h2>Task Not Completed</h2>
+              )}
+            </div>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
